485e3485ed3fee4f04be3487dd8eaaa738338de3
mdcoon <mdcoon1@yahoo.com>
2019-05-06 09:47:57 -0400

    Trying to get server to run in heroku for dashboard

--- packages/dashboard/build/service-worker.js
+++ packages/dashboard/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tellor-io/tellorscan/tree/master/packages/dashboard/precache-manifest.00a8f2e5eed4be8253488c5d6c47778b.js"
+  "/precache-manifest.165070f39daf3fcdd7566900a96ef2cb.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/tellor-io/tellorscan/tree/master/packages/dashboard/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
