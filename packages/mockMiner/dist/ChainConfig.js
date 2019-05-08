24def82b0b945fab05884bfc4bda6f4ea2b2f5fc
mdcoon <mdcoon1@yahoo.com>
2019-05-08 05:28:41 -0400

    added dist to ignore

--- packages/mockMiner/dist/ChainConfig.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var MASTER_CONTRACT = exports.MASTER_CONTRACT = "0x2B63d6e98E66C7e9fe11225Ba349B0B33234D9A2".toLowerCase();
-//# sourceMappingURL=ChainConfig.js.map
