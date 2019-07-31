fa8c9fb85c423ee39786e0f0208475dc006f4976
themandalore <fett.nicholas@yahoo.com>
2019-07-31 13:12:33 -0400

    changedAddresses

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,8 +4,8 @@ module.exports = {
       name: "js-mock-miner",
       script: "./tellorscan/packages/mockMiner/dist/index.js",
       env: {
-        WEB3_URL: "fill in",
-        CONTRACT_ADDRESS: "fill in",
+        WEB3_URL: "wss://rinkeby.infura.io/ws/v3/fbb0461697da49bb8c2815a43b97fc45",
+        CONTRACT_ADDRESS: "0x3f1571E4DFC9f3A016D90e0C9824C56fD8107a3e",
         MINE_SLEEP_CYCLE: '10m',
         REQUEST_RATE: '1h',
         QUERY_STR: "json(https://api.gdax.com/products/ETH-USD/ticker).price",
--- packages/dashboard/src/Constants/chain/web3Config.js
+++ packages/dashboard/src/Constants/chain/web3Config.js
@@ -1,7 +1,7 @@
 
 export const DEFAULT_MASTER_CONTRACT = "0x2B63d6e98E66C7e9fe11225Ba349B0B33234D9A2".toLowerCase();
 export const RINKEBY_v1 = "0xD43051dc258BE91f02B7C3d2c08fFe0085503BC2".toLowerCase();
-export const RINKEBY = "0xDae06771E342fc7A8BddBe9b159bB9fa8cE4D626".toLowerCase();
+export const RINKEBY = "0x3f1571E4DFC9f3A016D90e0C9824C56fD8107a3e".toLowerCase();
 
 
 export default {
