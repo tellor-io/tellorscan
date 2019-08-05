2e29973df9d02eec284e582ef6297299f91d4dad
themandalore <fett.nicholas@yahoo.com>
2019-08-05 11:13:11 -0400

    right main

--- packages/dashboard/src/Constants/chain/web3Config.js
+++ packages/dashboard/src/Constants/chain/web3Config.js
@@ -2,7 +2,7 @@
 export const DEFAULT_MASTER_CONTRACT = "0x2B63d6e98E66C7e9fe11225Ba349B0B33234D9A2".toLowerCase();
 export const RINKEBY_v1 = "0xD43051dc258BE91f02B7C3d2c08fFe0085503BC2".toLowerCase();
 export const RINKEBY = "0x3f1571E4DFC9f3A016D90e0C9824C56fD8107a3e".toLowerCase();
-export const MAINNET = "0xbAF31Bbbba24AF83c8a7a76e16E109d921E4182a".toLowerCase();
+export const MAINNET = "0x0Ba45A8b5d5575935B8158a88C631E9F9C95a2e5".toLowerCase();
 
 export default {
   private_address: DEFAULT_MASTER_CONTRACT,
