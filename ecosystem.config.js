89796342618b9e131c59658b139e6f9efff4f0b6
themandalore <fett.nicholas@yahoo.com>
2019-08-05 10:52:06 -0400

    main

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,8 +4,8 @@ module.exports = {
       name: "js-mock-miner",
       script: "./tellorscan/packages/mockMiner/dist/index.js",
       env: {
-        WEB3_URL: "wss://rinkeby.infura.io/ws/v3/fbb0461697da49bb8c2815a43b97fc45",
-        CONTRACT_ADDRESS: "0x3f1571E4DFC9f3A016D90e0C9824C56fD8107a3e",
+        WEB3_URL: "wss://mainnet.infura.io/ws/v3/fbb0461697da49bb8c2815a43b97fc45",
+        CONTRACT_ADDRESS: "0xbAF31Bbbba24AF83c8a7a76e16E109d921E4182a",
         MINE_SLEEP_CYCLE: '10m',
         REQUEST_RATE: '1h',
         QUERY_STR: "json(https://api.gdax.com/products/ETH-USD/ticker).price",
--- packages/dashboard/src/Constants/chain/web3Config.js
+++ packages/dashboard/src/Constants/chain/web3Config.js
@@ -2,9 +2,10 @@
 export const DEFAULT_MASTER_CONTRACT = "0x2B63d6e98E66C7e9fe11225Ba349B0B33234D9A2".toLowerCase();
 export const RINKEBY_v1 = "0xD43051dc258BE91f02B7C3d2c08fFe0085503BC2".toLowerCase();
 export const RINKEBY = "0x3f1571E4DFC9f3A016D90e0C9824C56fD8107a3e".toLowerCase();
-
+export const MAINNET = "0xbAF31Bbbba24AF83c8a7a76e16E109d921E4182a".toLowerCase();
 
 export default {
   private_address: DEFAULT_MASTER_CONTRACT,
-  rinkeby_address: RINKEBY
+  rinkeby_address: RINKEBY,
+  mainnet_address: MAINNET
 }
