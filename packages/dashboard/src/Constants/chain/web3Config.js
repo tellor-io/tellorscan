96d42dcda52bc53c0c7137cf8c66c8e8e311404b
themandalore <fett.nicholas@yahoo.com>
2020-03-06 13:59:03 -0500

    rinkeby add

--- packages/dashboard/src/Constants/chain/web3Config.js
+++ packages/dashboard/src/Constants/chain/web3Config.js
@@ -2,7 +2,7 @@
 export const DEFAULT_MASTER_CONTRACT = "0x7ddc408c0cd13d3543156ae2bc5772c56e91aa0f".toLowerCase();
 // export const RINKEBY_v1 = "0xD43051dc258BE91f02B7C3d2c08fFe0085503BC2".toLowerCase();
 // export const RINKEBY_v2 = "0x3f1571E4DFC9f3A016D90e0C9824C56fD8107a3e".toLowerCase();
-export const RINKEBY = "0x724D1B69a7Ba352F11D73fDBdEB7fF869cB22E19".toLowerCase();
+export const RINKEBY = "0xFe41Cb708CD98C5B20423433309E55b53F79134a".toLowerCase();
 export const MAINNET = "0x0Ba45A8b5d5575935B8158a88C631E9F9C95a2e5".toLowerCase();
 
 export default {
