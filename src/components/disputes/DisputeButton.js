091dbb179f1406d7d58806a2b1b56b907d642bae
skuhlmann <samkuhlmann@gmail.com>
2020-05-25 16:53:04 -0600

    stubs out the vote and sipute modals

--- src/components/disputes/DisputeButton.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Button } from 'antd';
-
-const DisputeButton = ({ value }) => {
-  return <Button type="primary">Dispute</Button>;
-};
-
-export default DisputeButton;
