0d79502171f37ef4f302453a9274e52959e4952f
oovg <office@vengist.com>
2020-05-28 13:03:04 -0700

    starts loader animation

--- src/components/shared/Loader.js
+++ src/components/shared/Loader.js
@@ -16,7 +16,16 @@ const LoadingDiv = styled.div`
 const Loader = () => {
   return (
     <LoadingDiv>
-      <LoadingOutlined />
+      <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+      <lottie-player
+        src="https://assets6.lottiefiles.com/packages/lf20_QTYe4e.json"
+        background="transparent"
+        speed="1"
+        style={{ width: '300px', height: '300px' }}
+        loop
+        controls
+        autoplay
+      ></lottie-player>
     </LoadingDiv>
   );
 };
