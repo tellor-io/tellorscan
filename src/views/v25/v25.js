d2fdc9389d8b9f6c6965c671cd353df9020f845c
JG Carvalho <jg@jgcarv.com>
2020-10-15 15:16:20 -0300

    cleanup

--- src/views/v25/v25.js
+++ src/views/v25/v25.js
@@ -1,16 +1,7 @@
-import React, { useState } from 'react';
-
-import { GET_ALL_EVENTS } from 'utils/queries';
-import CurrentMiningEvent from 'components/mining-events/CurrentMiningEvent';
-import AllMiningEvents from 'components/mining-events/AllMiningEvents';
-import GraphFetch from 'components/shared/GraphFetch';
-import CurrentEventFetch from 'components/mining-events/CurrentEventFetch';
-import OpenDisputesFetch from 'components/disputes/OpenDiputesFetch';
+import React from 'react';
 import VoteForm from 'components/votes/VoteForm';
 
 const Mining = () => {
-  const [currentEvent, setCurrentEvent] = useState();
-  const [events, setEvents] = useState();
 
   return (
     <>
