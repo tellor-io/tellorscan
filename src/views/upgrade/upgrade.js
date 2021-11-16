a1f0878f95c19b0d290e311bafc9edcafbd2166b
Joshua Samaniego <joshuadsamaniego@gmail.com>
2021-11-16 11:21:00 -0700

    adds basic vote page at route /vote

--- src/views/upgrade/upgrade.js
+++ src/views/upgrade/upgrade.js
@@ -1,32 +1,98 @@
 import React from 'react';
-import VotingForm from 'components/voting/VotingForm';
 
-const Upgrade = () => {
+//TODOS:
+
+// [] Get current TRB value from address to see if they have enough to vote.
+//////// [] currentAddress.TRB token amount
+//////// [] what's the TRB amount needed to vote?
+// [] Be able to handle Rinkeby and Mainnet or just Rinkeby?
+// [] Have a button that votes on the contract method when clicked.
+//////// [] whats the contract method?
+// [] Find out what the method needs as parameters
+//////// [] vote Id 78, right?
+// [] Have a way to display current state of vote?
+//////// [] curent votes for/against?
+// [] Use web3 for this
+
+// Use iTellorABI?
+
+const Upgrade = ({ currentUser }) => {
+  //Globals
+  const voteId = 78;
+
+  //Handlers
+  const handleVote = async (bool) => {
+    if (currentUser) {
+      if (currentUser.address && currentUser.network === 1) {
+        const vote = await currentUser.contracts.instance.methods
+          .vote(voteId, bool)
+          .send({ from: currentUser.address });
+        // console.log(vote);
+        return vote;
+      } else if (currentUser.address && currentUser.network === 4) {
+        alert('Please sign in on Mainnet to vote!');
+      }
+    } else {
+      alert(
+        'Please sign in with MetaMask, via the green connect button, to vote!',
+      );
+    }
+  };
+
   return (
     <>
       <div className="Hero">
-        <div className="View">
+        <div
+          className="View"
+          style={{
+            padding: '100px 180px',
+          }}
+        >
           <h1 style={{ fontSize: 60, alignContent: 'center' }}>
-            Tellor 2.6 Proposal!
+            Tellor X Proposal!
           </h1>
-          <p>
-            TLDR: Fix the difficulty adjustment to be based 4th slot, allowing
-            slots 1-4 to be fairly mined. The difficulty for the 5th slot is
-            always 1, making it a proof-of-stake slot.
-          </p>
-          <div style={{ margin: 20 }}>
-            <VotingForm dispute={{ disputeId: 54 }} />
-          </div>
-          <p>
-            <a
-              href={
-                'https://github.com/tellor-io/TIPs/blob/main/TIPs/TIP-6%20Tellor2.6.md'
-              }
-            >
-              {' '}
-              See Full Proposal{' '}
+          <h2>
+            TL;DR: Tellor is upgrading to TellorX. Vote in support of this, just
+            do it!
+          </h2>
+          <h3>
+            <a href="https://www.tellor.io/static/media/tellorX-whitepaper.f6527d55.pdf">
+              Link to Whitepaper
             </a>
-          </p>
+          </h3>
+          <div
+            style={{
+              marginTop: '50px',
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}
+          >
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              <h2>Click here to vote in favor of the TellorX Upgrade</h2>
+              <button onClick={() => handleVote(true)} className="ant-btn">
+                Vote in Favor
+              </button>
+            </div>
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              <h2>Click here to vote in opposition of the TellorX Upgrade</h2>
+              <button onClick={() => handleVote(false)} className="ant-btn">
+                Vote in Opposition
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </>
